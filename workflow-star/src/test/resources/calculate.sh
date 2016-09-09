ac767298c3daf58bd2600a48bbba5623b5547f39
Peter Ruzanov <pruzanov@gmail.com>
2016-09-09 09:24:45 -0400

    Debugging Reg Testing...

--- workflow-star/src/test/resources/calculate.sh
+++ workflow-star/src/test/resources/calculate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd $1
+echo $1; \
 module load samtools 2>/dev/null; \
 find . -regex './data/.*\.bam$' -exec sh -c " samtools view -H {} | grep '^@RG' |  tr '\n' '\t'; echo " \; |  sort | uniq | tr '\t' '\n'; \
 find . -regex './data/.*\.bam$' -exec sh -c " samtools flagstat {} | tr '\n' '\t'; echo " \; |  sort | uniq | tr '\t' '\n'
