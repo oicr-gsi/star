ecfbeb6518f5cb29ef73d47811ad383056a6ec84
Peter Ruzanov <pruzanov@gmail.com>
2016-09-09 09:58:52 -0400

    Fixed Reg Tests for Workflow

--- workflow-star/src/test/resources/calculate.sh
+++ workflow-star/src/test/resources/calculate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $1
-echo $1; \
+#echo $1; \ # Debugging
 module load samtools 2>/dev/null; \
-find . -regex './data/.*\.bam$' -exec sh -c " samtools view -H {} | grep '^@RG' |  tr '\n' '\t'; echo " \; |  sort | uniq | tr '\t' '\n'; \
-find . -regex './data/.*\.bam$' -exec sh -c " samtools flagstat {} | tr '\n' '\t'; echo " \; |  sort | uniq | tr '\t' '\n'
+find . -regex './*\.bam$' -exec sh -c " samtools view -H {} | grep '^@RG' |  tr '\n' '\t'; echo " \; |  sort | uniq | tr '\t' '\n'; \
+find . -regex './*\.bam$' -exec sh -c " samtools flagstat {} | tr '\n' '\t'; echo " \; |  sort | uniq | tr '\t' '\n'
