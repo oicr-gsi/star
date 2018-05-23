c2fbad63b7d356e64cf0c0eda23cac97b5494c94
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2018-05-23 15:24:26 -0400

    Formatting and move ReadGroupData class

--- decider-star/src/main/java/ca/on/oicr/pde/deciders/STARDecider.java
+++ decider-star/src/main/java/ca/on/oicr/pde/deciders/STARDecider.java
@@ -1,6 +1,5 @@
 package ca.on.oicr.pde.deciders;
 
-import com.google.common.collect.MoreCollectors;
 import com.google.common.collect.Sets;
 import java.util.*;
 import net.sourceforge.seqware.common.hibernate.FindAllTheFiles;
@@ -14,6 +13,7 @@ import net.sourceforge.seqware.common.util.Log;
  * @author rtahir
  */
 public class STARDecider extends OicrDecider {
+
     private String index_dir;
     private String produce_transcriptome_bam = "true";
     private String RGCM = "";
@@ -21,29 +21,29 @@ public class STARDecider extends OicrDecider {
     private String read1_adapterTrim = "AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC";
     private String read2_adapterTrim = "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT";
     private String numOfThreads = "6";
-    private String starMemory   = "16000";
+    private String starMemory = "16000";
     private String queue = "";
-    
+
     private static final String OICR = "OICR";
     private static final String ILLUMINA = "Illumina"; //If we don't have this passed as parameter, we assume Illumina
     private Set<String> allowedTemplateTypes;
-    
+
     private String inputFile1;
     private String inputFile2;
     private ReadGroupData readGroupDataForWorkflowRun;
-    
+
     public STARDecider() {
         super();
         parser.accepts("ini-file", "Optional: the location of the INI file.").withRequiredArg();
         parser.accepts("index-dir", "reference index dir").withRequiredArg();
         parser.accepts("produce-transcriptome-bam", "Optional*. Set either to true or false for production of additional files").withRequiredArg();
-        
+
         //star aln
         parser.accepts("star-aln-threads", "Optional: STAR threads, default is 6.").withRequiredArg();
         parser.accepts("star-aln-mem-mb", "Optional: STAR allocated memory Mb, default is 16000.").withRequiredArg();
         parser.accepts("additionalStarParams", "Optional: Star additional parameters").withRequiredArg();
         //RG parameters
-        parser.accepts("rg-library",  "Optional: RG Library, (LB).").withRequiredArg();
+        parser.accepts("rg-library", "Optional: RG Library, (LB).").withRequiredArg();
         parser.accepts("rg-platform", "Optional: RG Platform, default illumina.").withRequiredArg();
         parser.accepts("rg-platform-unit", "Optional: RG Platform unit (PU).").withRequiredArg();
         parser.accepts("rg-sample-name", "Optional: RG Sample name (SM).").withRequiredArg();
@@ -62,13 +62,13 @@ public class STARDecider extends OicrDecider {
         this.setHeadersToGroupBy(Arrays.asList(FindAllTheFiles.Header.IUS_SWA));
 
         //allows anything defined on the command line to override the defaults here.
-        if (this.options.has("index-dir")){
+        if (this.options.has("index-dir")) {
             this.index_dir = options.valueOf("index-dir").toString();
         } else {
             Log.error("index-dir needs to be set");
             System.exit(1);
         }
-        
+
         if (this.options.has("produce-transcriptome-bam")) {
             this.produce_transcriptome_bam = options.valueOf("produce-transcriptome-bam").toString();
         }
@@ -79,7 +79,7 @@ public class STARDecider extends OicrDecider {
         } else {
             this.RGCM = OICR;
         }
-        
+
         //star
         if (this.options.has("star-threads")) {
             this.numOfThreads = options.valueOf("star-threads").toString();
@@ -145,12 +145,12 @@ public class STARDecider extends OicrDecider {
             Log.error("The Decider was not able to find both R1 and R2 fastq files for paired sequencing alignment, WON'T RUN");
             return new ReturnValue(ReturnValue.INVALIDPARAMETERS);
         }
-        
+
         readGroupDataForWorkflowRun = new ReadGroupData(files.get(inputFile1), files.get(inputFile2));
-        
+
         return super.doFinalCheck(commaSeparatedFilePaths, commaSeparatedParentAccessions);
     }
-        
+
     @Override
     protected boolean checkFileDetails(ReturnValue returnValue, FileMetadata fm) {
         Log.debug("CHECK FILE DETAILS:" + fm);
@@ -174,10 +174,10 @@ public class STARDecider extends OicrDecider {
         iniFileMap.put("input_file_2", inputFile2);
         iniFileMap.put("index_dir", this.index_dir);
         iniFileMap.put("produce_transcriptome_bam", this.produce_transcriptome_bam);
-        
+
         //For RG setting
         String RGLB = this.options.has("rg-library") ? options.valueOf("rg-library").toString() : readGroupDataForWorkflowRun.getRGLB();
-        String RGPL = this.options.has("rg-platform") ? options.valueOf("rg-platform").toString() : ILLUMINA; 
+        String RGPL = this.options.has("rg-platform") ? options.valueOf("rg-platform").toString() : ILLUMINA;
         String RGPU = this.options.has("rg-platform_unit") ? options.valueOf("rg-platform-unit").toString() : readGroupDataForWorkflowRun.getRGPU();
         String RGSM = this.options.has("rg-sample-name") ? options.valueOf("rg-sample-name").toString() : readGroupDataForWorkflowRun.getRGSM();
 
@@ -200,7 +200,7 @@ public class STARDecider extends OicrDecider {
 
         return iniFileMap;
     }
-   
+
     public static void main(String args[]) {
 
         List<String> params = new ArrayList<String>();
@@ -213,65 +213,4 @@ public class STARDecider extends OicrDecider {
 
     }
 
-    private class ReadGroupData {
-
-        private final String tissueType;
-        private final String RGLB;
-        private final String RGPU;
-        private final String RGSM;
-        private final String ius_accession;
-        private final String sequencer_run_name;
-        private final String barcode;
-        private final String lane;
-
-        public ReadGroupData(FileAttributes... inputFileAttrs) {
-            List<FileAttributes> attrs = Arrays.asList(inputFileAttrs);
-            this.tissueType = attrs.stream().map(a -> a.getLimsValue(Lims.TISSUE_TYPE)).distinct().collect(MoreCollectors.onlyElement());
-            this.lane = attrs.stream().map(a -> a.getLane().toString()).distinct().collect(MoreCollectors.onlyElement());
-            this.RGLB = attrs.stream().map(a -> a.getLibrarySample()).distinct().collect(MoreCollectors.onlyElement());
-            this.RGPU = attrs.stream().map(a -> a.getSequencerRun() + "_" + this.lane + "_" + a.getBarcode()).distinct().collect(MoreCollectors.onlyElement());
-            this.RGSM = attrs.stream().map(a -> {
-                String rgsm = a.getDonor() + "_" + this.tissueType;
-                if (a.getLimsValue(Lims.GROUP_ID) != null && !a.getLimsValue(Lims.GROUP_ID).isEmpty()) {
-                    rgsm += "_" + a.getLimsValue(Lims.GROUP_ID);
-                }
-                return rgsm;
-            }).distinct().collect(MoreCollectors.onlyElement());
-            this.ius_accession = attrs.stream().map(a -> a.getOtherAttribute(FindAllTheFiles.Header.IUS_SWA)).distinct().collect(MoreCollectors.onlyElement());
-            this.sequencer_run_name = attrs.stream().map(a -> a.getSequencerRun()).distinct().collect(MoreCollectors.onlyElement());
-            this.barcode = attrs.stream().map(a -> a.getBarcode()).distinct().collect(MoreCollectors.onlyElement());
-        }
-
-        public String getTissueType() {
-            return this.tissueType;
-        }
-
-        public String getRGLB() {
-            return RGLB;
-        }
-
-        public String getRGPU() {
-            return RGPU;
-        }
-
-        public String getRGSM() {
-            return RGSM;
-        }
-
-        public String getIus_accession() {
-            return ius_accession;
-        }
-
-        public String getSequencer_run_name() {
-            return sequencer_run_name;
-        }
-
-        public String getBarcode() {
-            return barcode;
-        }
-
-        public String getLane() {
-            return lane;
-        }
-    }
 }
