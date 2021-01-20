e323fab4644bfe5c271a1d21614e21b95d8c0a00
alexjfortuna <alexjfortuna@gmail.com>
2021-01-20 10:22:53 -0500

    update calculate script

--- calculate.sh
+++ calculate.sh
@@ -6,7 +6,17 @@ set -o pipefail
 cd $1
 
 module load samtools/1.9 2>/dev/null
-find . -regex '.*\.bam$' -exec samtools flagstat {} \;
-find . -regex '.*\.tab$' -exec wc -l {} \;
-find . -regex '.*\.junction$' -exec wc -l {} \;
-ls | sed 's/.*\.//' | sort | uniq -c
+
+ls | sort
+
+find . -regex '.*\.bam$' -exec samtools view -H {} \; | grep '^@RG' | sort
+
+find . -regex '.*\.bam$' -exec samtools flagstat {} \; | sort
+
+find . -regex '.*\.bam$' -exec /bin/bash -c "samtools view {} | md5sum" \; | sort
+
+find . -regex '.*\.bai$' -exec /bin/bash -c "samtools view {} | md5sum" \; | sort
+
+find . -regex '.*\.Chimeric.out.junction$' -exec /bin/bash -c "cat {} | md5sum" \; | sort
+
+find . -regex '.*\.ReadsPerGene.out.tab$' -exec /bin/bash -c "cat {} | md5sum" \; | sort
