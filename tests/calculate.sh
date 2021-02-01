30f86c2e5f842754b103ef431c14a91f46062a76
alexjfortuna <alexjfortuna@gmail.com>
2021-02-01 16:30:45 -0500

    update tests

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -13,10 +13,6 @@ find . -regex '.*\.bam$' -exec samtools view -H {} \; | grep '^@RG' | sort
 
 find . -regex '.*\.bam$' -exec samtools flagstat {} \; | sort
 
-find . -regex '.*\.bam$' -exec /bin/bash -c "samtools view {} | md5sum" \; | sort
+find . -regex '.*\.tab$' -exec wc -l {} \;
 
-find . -regex '.*\.bai$' -exec /bin/bash -c "samtools view {} | md5sum" \; | sort
-
-find . -regex '.*\.Chimeric.out.junction$' -exec /bin/bash -c "cat {} | md5sum" \; | sort
-
-find . -regex '.*\.ReadsPerGene.out.tab$' -exec /bin/bash -c "cat {} | md5sum" \; | sort
+find . -regex '.*\.junction$' -exec wc -l {} \;
